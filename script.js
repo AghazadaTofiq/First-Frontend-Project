a32031346342bd4d9965ec1a20695e85a20606d0
Letofig <tofiq@goinos.com>
2023-12-10 18:34:42 +0400

    Polished

--- script.js
+++ script.js
@@ -42,30 +42,30 @@ function backFunction()
 
 
 const container = document.getElementById('PartnerLogos');
-    const images = container.querySelectorAll('img');
-    let currentPosition = 0;
+const images = container.querySelectorAll('img');
+let currentPosition = 0;
 
-    function moveImages() {
-      // Calculate the total width of displayed images
-      const totalWidth = Array.from(images).reduce((sum, image) => sum + image.offsetWidth + 20, 0);
+function moveImages() {
+  // Calculate the total width of displayed images
+  const totalWidth = 0;
 
-      currentPosition -= images[0].offsetWidth + 20; // Move to the left by one image width + margin
+  currentPosition -= images[0].offsetWidth + 20; // Move to the left by one image width + margin
 
-      images.forEach(image => {
-        image.style.transform = `translateX(${currentPosition}px)`;
-      });
+  //images.forEach(image => {
+    //image.style.transform = `translateX(${currentPosition}px)`;
+  //});
+  
+  // Check if the first image has moved out of view
+  if (currentPosition < -totalWidth) {
+    currentPosition += images[0].offsetWidth + 20; // Move to the right by one image width + margin
+    container.appendChild(container.firstElementChild); // Move the first image to the end
+  }
+}
 
-      // Check if the first image has moved out of view
-      if (currentPosition < -totalWidth) {
-        currentPosition += images[0].offsetWidth + 20; // Move to the right by one image width + margin
-        container.appendChild(container.firstElementChild); // Move the first image to the end
-      }
-    }
+// Adjust the interval based on the desired speed
+const interval = setInterval(moveImages, 2000); // Adjust the interval as needed
 
-    // Adjust the interval based on the desired speed
-    const interval = setInterval(moveImages, 2000); // Adjust the interval as needed
-
-    // Optionally, you can stop the animation after a certain duration
-    setTimeout(() => {
-      clearInterval(interval);
-    }, 30000); // Stop after 30 seconds (adjust as needed)
+// Optionally, you can stop the animation after a certain duration
+setTimeout(() => {
+  clearInterval(interval);
+}, 30000); // Stop after 30 seconds (adjust as needed)
